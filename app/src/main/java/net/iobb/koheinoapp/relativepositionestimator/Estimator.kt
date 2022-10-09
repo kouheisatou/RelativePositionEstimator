8f08f11170472153f0fecdc7cd06306279777ac5
kouheisatou <koheisato0420@gmail.com>
2022-10-09 11:23:23 +0900

    加速度の分散が小さい時は動いていないと判定し、強制的に速度を0にする

--- app/src/main/java/net/iobb/koheinoapp/relativepositionestimator/Estimator.kt
+++ app/src/main/java/net/iobb/koheinoapp/relativepositionestimator/Estimator.kt
@@ -1,5 +1,7 @@
 package net.iobb.koheinoapp.relativepositionestimator
 
+import kotlin.math.pow
+
 private const val OUTLIER_THRESHOLD = 0.1
 private const val NUMBER_OF_CORRECTION_SAMPLING = 1000
 
@@ -24,8 +26,8 @@ class Estimator {
             }
 
         currentAcceleration[0] = samplingItem.getAX() + (correctionVector?.get(0) ?: 0f)
-        currentAcceleration[1] = samplingItem.getAX() + (correctionVector?.get(1) ?: 0f)
-        currentAcceleration[2] = samplingItem.getAX() + (correctionVector?.get(2) ?: 0f)
+        currentAcceleration[1] = samplingItem.getAY() + (correctionVector?.get(1) ?: 0f)
+        currentAcceleration[2] = samplingItem.getAZ() + (correctionVector?.get(2) ?: 0f)
 
         val aX: Float = currentAcceleration[0]
         val aY: Float = currentAcceleration[1]
@@ -36,12 +38,12 @@ class Estimator {
         val vZ0: Float = currentVelocity[2]
 
         val x0: Float = currentPosition[0]
-        val y0: Float = currentPosition[0]
-        val z0: Float = currentPosition[0]
+        val y0: Float = currentPosition[1]
+        val z0: Float = currentPosition[2]
 
         currentVelocity[0] = aX * dt + vX0
-        currentVelocity[1] = aX * dt + vY0
-        currentVelocity[2] = aX * dt + vZ0
+        currentVelocity[1] = aY * dt + vY0
+        currentVelocity[2] = aZ * dt + vZ0
 
         currentPosition[0] = 0.5f * aX * dt * dt + vX0 * dt + x0
         currentPosition[1] = 0.5f * aY * dt * dt + vY0 * dt + y0
@@ -50,6 +52,22 @@ class Estimator {
         println("$aX, $vX0, $x0, $dt")
 
         samplings.add(samplingItem)
+
+        // 動いていない判定
+        val accelerationsX = mutableListOf<Float>()
+        val accelerationsY = mutableListOf<Float>()
+        val accelerationsZ = mutableListOf<Float>()
+        for(sampling in samplings){
+            accelerationsX.add(sampling.getAX())
+            accelerationsY.add(sampling.getAY())
+            accelerationsZ.add(sampling.getAZ())
+        }
+        if(variance(accelerationsX) < 0.01 && variance(accelerationsY) < 0.01 && variance(accelerationsZ) < 0.01){
+            // 加速度の分散が小さい時には動いていないと判定し速度を0にする
+            currentVelocity[0] = 0f
+            currentVelocity[1] = 0f
+            currentVelocity[2] = 0f
+        }
 
         if (samplings.size > NUMBER_OF_RETAINING_SAMPLINGS) {
             samplings.removeAt(0)
@@ -98,4 +116,21 @@ class Estimator {
 
         correctionVector = result
     }
+}
+
+fun average(values: List<Float>): Float{
+    var sum = 0f
+    for(value in values){
+        sum += value
+    }
+    return sum / values.size
+}
+
+fun variance(values: List<Float>): Float{
+    var sum = 0f
+    val ave = average(values)
+    for(value in values){
+        sum += (value - ave).toDouble().pow(2.0).toFloat()
+    }
+    return sum / values.size
 }
